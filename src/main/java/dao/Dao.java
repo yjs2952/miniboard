d4e130d8991c0b8f5f05ccedf1caad41b1e13c0a
NaMaanKim <abcde5178@naver.com>
2018-10-08 16:29:29 +0900

    게시판 Dao

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -1,4 +1,40 @@
 package dao;
 
 public class Dao {
+    private String UserID;
+    private String UserPassword;
+    private String UserEmaile;
+    private String UserName;
+
+    public String getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(String userID) {
+        UserID = userID;
+    }
+
+    public String getUserPassword() {
+        return UserPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        UserPassword = userPassword;
+    }
+
+    public String getUserEmaile() {
+        return UserEmaile;
+    }
+
+    public void setUserEmaile(String userEmaile) {
+        UserEmaile = userEmaile;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
 }
